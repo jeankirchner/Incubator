b71379a5e7798aaf5a9d3e73afc00227254f8a03
Jean Kirchner <jeankirchner@gmail.com>
2020-02-18 21:57:26 -0300

    retrying clojure

--- clojure/brave/clojure-noob/src/clojure_noob/core.clj
+++ clojure/brave/clojure-noob/src/clojure_noob/core.clj
@@ -1,11 +1,7 @@
 (ns clojure-noob.core
   (:gen-class))
 
-(defn train
-  []
-  (println "Cho cho2!"))
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println args))
+  (println "Hello, World!"))
